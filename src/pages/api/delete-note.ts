ba4cd31e739eb705d48fd362050d0c5929dadbab
lunarfrm777 <photoslune8@gmail.com>
2026-06-02 14:28:37 -0400

    added some code into delete-note.ts to get note from database with matching id

--- src/pages/api/delete-note.ts
+++ src/pages/api/delete-note.ts
@@ -1,4 +1,18 @@
 // this is the endpoint for submitting the form in notes/[slug].astro to actually go get the slug that matches the id and gets
 // rid of it from sanity aka the db
 
-await client.delete(id)
+import { client } from '../../lib/sanity'
+
+export async function POST({ request }) {
+  const form = await request.formData()
+  const id = form.get('id')
+
+  await client.delete(id)
+
+  return new Response(null, {
+    status: 303,
+    headers: {
+      Location: '/'
+    }
+  })
+}
